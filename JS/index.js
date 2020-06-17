1af4b6430132a85b5504e0a868eaf00177a20e3e
LivingSaint123 <62385684+LivingSaint123@users.noreply.github.com>
2020-06-17 11:15:38 +0100

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+
+function calc() {
+    let Price = document.getElementById("Price").value;
+    let Quantity = document.getElementById("Quantity").value;
+    alert(`your total amount is: ${Price * Quantity} `);
+}
